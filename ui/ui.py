a73655600f3936f9bc71f99db92dffb03cf9e3c0
Q <QuintenMeottel@Gmail.com>
2024-03-24 13:59:25 -0500

    fix frames not spanning the entire width on the master

--- ui/ui.py
+++ ui/ui.py
@@ -4,23 +4,28 @@ import tkinter as tk
 class MainWindow(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master, width=800, height=600)
-        self.grid()
+        self.grid(sticky=tk.EW)
         self.create_widgets()
+        # set master and self to have '0' columns.
+        if self.master:
+            self.master.grid_columnconfigure(0, weight=1)
+        
+        self.grid_columnconfigure(0, weight=1)
 
     def create_widgets(self):
-        # self.grid_columnconfigure(2, weight=1)
         
         self.quit_button = tk.Button(self, text='Quit', command=self.quit)
-        self.quit_button.grid(sticky=tk.EW)
+        self.quit_button.grid(sticky=tk.NSEW)
 
         self.current_task_labelframe = tk.LabelFrame(self, padx=2, pady=2, text='Current Task')
-        self.current_task_labelframe.grid()
+        self.current_task_labelframe.grid_columnconfigure(0, weight=1)
+        self.current_task_labelframe.grid(sticky=tk.EW)
 
         self.current_task_label = tk.Label(self.current_task_labelframe, text="Placeholder Text", anchor=tk.W, width=50)
-        self.current_task_label.pack()
+        self.current_task_label.grid(sticky=tk.EW)
         
         self.task_button_frame = tk.Frame(self, padx=2, pady=2)
-        self.task_button_frame.grid(sticky=tk.W)
+        self.task_button_frame.grid(sticky=tk.E)
         
         self.get_task_button = tk.Button(self.task_button_frame, text="Get Task")
         self.get_task_button.grid(column=0, row=0)
