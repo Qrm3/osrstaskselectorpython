929fe1612f1dd9c567e1b181bdd6833f9cc26e3c
Q <QuintenMeottel@Gmail.com>
2024-04-06 19:38:36 -0500

    Add labels and textboxes to new tasks

--- ui/ui.py
+++ ui/ui.py
@@ -18,7 +18,7 @@ class MainWindow(tk.Frame):
         self.current_task_labelframe.grid_columnconfigure(0, weight=1)
         self.current_task_labelframe.grid(sticky=tk.EW)
 
-        self.current_task_label = tk.Label(self.current_task_labelframe, text="Placeholder Text", anchor=tk.W, width=50)
+        self.current_task_label = tk.Label(self.current_task_labelframe, text="Placeholder Text", anchor=tk.W, width=50, border='1')
         self.current_task_label.grid(sticky=tk.EW)
         
         # Task Buttons        
@@ -35,17 +35,28 @@ class MainWindow(tk.Frame):
         self.complete_task_button.grid(column=2, row=0)
 
         # New Task
-        self.new_task_labelframe = tk.LabelFrame(self, padx=2, pady=2, text='New Task')
-        self.new_task_labelframe.columnconfigure(0, weight=1)
+        self.new_task_labelframe = tk.LabelFrame(self, padx=2, pady=2, text="New Task")
+        self.new_task_labelframe.columnconfigure(1, weight=1)
         self.new_task_labelframe.grid(sticky=tk.EW)
         
-        self.new_task_textbox = tk.Text(self.new_task_labelframe, height=1, padx=2, pady=2, takefocus=0)
-        self.new_task_textbox.grid(sticky=tk.EW)
+        # Task Name Field
+        self.new_task_name_label = tk.Label(self.new_task_labelframe, text="Name:")
+        self.new_task_name_label.grid(column=0, row=0, sticky=tk.W)
+
+        self.new_task_name_text = tk.Text(self.new_task_labelframe, height=1, padx=2, pady=2, takefocus=0)
+        self.new_task_name_text.grid(column=1, row=0, sticky=tk.EW)
+
+        # Task Description field
+        self.new_task_description_label = tk.Label(self.new_task_labelframe, text="Description:")
+        self.new_task_description_label.grid(column=0, row=1, sticky=tk.W)
+        
+        self.new_task_description_text = tk.Text(self.new_task_labelframe, height=1, padx=2, pady=2, takefocus=0)
+        self.new_task_description_text.grid(column=1, row=1, sticky=tk.EW)
         
         # New Task buttons
-        self.new_task_button_frame = tk.Frame(self, padx=2, pady=2)
+        self.new_task_button_frame = tk.Frame(self.new_task_labelframe, padx=2, pady=2)
         self.new_task_button_frame.grid(sticky=tk.E)
         
-        self.create_task_button = tk.Button(self.new_task_button_frame, text='Create Task')
-        self.create_task_button.grid(column=0, row=0)
+        self.create_task_button = tk.Button(self, text="Create Task")
+        self.create_task_button.grid(sticky=tk.E)
         
